42dc3e2f41459b5950d8bf8f286c90bf0b94d6fb
Rik D.T. Janssen <121875841+RikDTJanssen@users.noreply.github.com>
2023-11-12 10:14:54 +0100

    Added renaming of nodes for faculties of Utrecht University

--- harvest_to_ricgraph_examples/batch_harvest.py
+++ harvest_to_ricgraph_examples/batch_harvest.py
@@ -38,24 +38,66 @@
 import os
 import ricgraph as rcg
 
+rename_nodes_first_group = {
+    'University: Universiteit Utrecht': 'Utrecht University',
+    'UU Faculty: Faculteit Betawetenschappen': 'Science',
+    'UU Faculty: Faculteit Diergeneeskunde': 'Veterinary Medicine',
+    'UU Faculty: Faculteit Geesteswetenschappen': 'Humanities',
+    'UU Faculty: Faculteit Geowetenschappen': 'Geosciences',
+    'UU Faculty: Faculteit REBO': 'Law, Economics and Governance',
+    'UU Faculty: Faculteit Sociale Wetenschappen': 'Social and Behavioural Sciences',
+    'UU Faculty: University College Utrecht': 'University College Utrecht',
+    'UU Faculties / Services: Utrecht University Library': 'Utrecht University Library',
+    'UU Faculties / Services: Corporate Offices': 'University Corporate Offices',
+}
+
+rename_nodes_second_group = {
+    'Science': 'UU Faculty: Science',
+    'Veterinary Medicine': 'UU Faculty: Veterinary Medicine',
+    'Humanities': 'UU Faculty: Humanities',
+    'Geosciences': 'UU Faculty: Geosciences',
+    'Law, Economics and Governance': 'UU Faculty: Law, Economics and Governance',
+    'Social and Behavioural Sciences': 'UU Faculty: Social and Behavioural Sciences',
+    'University College Utrecht': 'UU Faculty: University College Utrecht',
+    'Utrecht University Library': 'UU Faculties/Services: Utrecht University Library',
+    'University Corporate Offices': 'UU Faculties/Services: University Corporate Offices',
+}
+
+
+def rename_nodes(name: str, rename_table: dict):
+    for old_value in rename_table:
+        new_value = rename_table[old_value]
+        print('batch_harvest.py/rename_nodes(): Updating node name: "' + name + '" value from: "' + old_value + '" to: "' + new_value + '".')
+        node = rcg.update_node_value(name=name, old_value=old_value, new_value=new_value)
+        if node is None:
+            print('----> Error: update of value: "' + old_value + '" to: "' + new_value + '" failed.')
+            exit(2)
+
+    return
+
 
 # ###########################################################
 # Batch order 1: Preferred batch order for Utrecht University
 # ###########################################################
-status = os.system('python harvest_pure_to_ricgraph.py --empty_ricgraph yes --organization UU')
+status = os.system('python harvest_pure_to_ricgraph.py --empty_ricgraph yes --organization UU --harvest_projects yes')
 if status != 0: print('===>>> batch_harvest.py: error while executing previous script, status: ' + str(status) + '.'); exit(status)
 
-# Change the value of the node representing Utrecht University. For explanation see above.
+# Change the 'value' of some nodes, so they will have the same name as nodes in following harvests.
 graph = rcg.open_ricgraph()
 if graph is None:
     print('Ricgraph could not be opened in batch_harvest.py.')
     exit(2)
-node = rcg.update_node_value(name='ORGANIZATION_NAME',
-                             old_value='University: Universiteit Utrecht',
-                             new_value='Utrecht University')
-if node is None:
-    print('Update of node failed in batch_harvest.py.')
+rename_nodes(name='ORGANIZATION_NAME', rename_table=rename_nodes_first_group)
+rcg.close_ricgraph()
+
+status = os.system('python harvest_uustaffpages_to_ricgraph.py --empty_ricgraph no')
+if status != 0: print('===>>> batch_harvest.py: error while executing previous script, status: ' + str(status) + '.'); exit(status)
+
+graph = rcg.open_ricgraph()
+if graph is None:
+    print('Ricgraph could not be opened in batch_harvest.py.')
     exit(2)
+rename_nodes(name='ORGANIZATION_NAME', rename_table=rename_nodes_second_group)
 rcg.close_ricgraph()
 
 status = os.system('python harvest_yoda_datacite_to_ricgraph.py --empty_ricgraph no')
@@ -67,8 +109,6 @@ if status != 0: print('===>>> batch_harvest.py: error while executing previous s
 status = os.system('python harvest_openalex_to_ricgraph.py --empty_ricgraph no --organization UMCU')
 if status != 0: print('===>>> batch_harvest.py: error while executing previous script, status: ' + str(status) + '.'); exit(status)
 
-status = os.system('python harvest_uustaffpages_to_ricgraph.py --empty_ricgraph no')
-if status != 0: print('===>>> batch_harvest.py: error while executing previous script, status: ' + str(status) + '.'); exit(status)
 
 # status = os.system('python harvest_pure_to_ricgraph.py --empty_ricgraph no --organization VU --harvest_projects yes')
 # if status != 0: print('===>>> batch_harvest.py: error while executing previous script, status: ' + str(status) + '.'); exit(status)
