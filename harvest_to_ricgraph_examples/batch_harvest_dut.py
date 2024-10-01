33f8ee3075545ed448be801a93c9d3ad4e2601e2
Rik D.T. Janssen <121875841+RikDTJanssen@users.noreply.github.com>
2024-10-01 08:42:45 +0200

    Renamed script from batch_harvest_tud.py to batch_harvest_dut.py

--- harvest_to_ricgraph_examples/batch_harvest_dut.py
+++ harvest_to_ricgraph_examples/batch_harvest_dut.py
@@ -32,7 +32,7 @@ rename_nodes_first_group = {
 def rename_nodes(name: str, rename_table: dict):
     for old_value in rename_table:
         new_value = rename_table[old_value]
-        print('batch_harvest_tud.py/rename_nodes(): Updating node name: "' + name + '" value from: "' + old_value + '" to: "' + new_value + '".')
+        print('batch_harvest_dut.py/rename_nodes(): Updating node name: "' + name + '" value from: "' + old_value + '" to: "' + new_value + '".')
         node = rcg.update_node_value(name=name, old_value=old_value, new_value=new_value)
         if node is None:
             print('----> Error: update of value: "' + old_value + '" to: "' + new_value + '" failed.')
@@ -44,18 +44,18 @@ def rename_nodes(name: str, rename_table: dict):
 # Batch order for Delft University of Technology
 # ###########################################################
 status = os.system('../bin/python harvest_pure_to_ricgraph.py --empty_ricgraph yes --organization DUT --harvest_projects yes')
-if status != 0: print('===>>> batch_harvest_tud.py: error while executing previous script, status: ' + str(status) + '.'); exit(status)
+if status != 0: print('===>>> batch_harvest_dut.py: error while executing previous script, status: ' + str(status) + '.'); exit(status)
 
 # Change the 'value' of some nodes, so they will have the same name as nodes in following harvests.
 graph = rcg.open_ricgraph()
 if graph is None:
-    print('Ricgraph could not be opened in batch_harvest_tud.py.')
+    print('Ricgraph could not be opened in batch_harvest_dut.py.')
     exit(2)
 rename_nodes(name='ORGANIZATION_NAME', rename_table=rename_nodes_first_group)
 rcg.close_ricgraph()
 
 status = os.system('../bin/python harvest_rsd_to_ricgraph.py --empty_ricgraph no --organization DUT')
-if status != 0: print('===>>> batch_harvest_tud.py: error while executing previous script, status: ' + str(status) + '.'); exit(status)
+if status != 0: print('===>>> batch_harvest_dut.py: error while executing previous script, status: ' + str(status) + '.'); exit(status)
 status = os.system('../bin/python harvest_openalex_to_ricgraph.py --empty_ricgraph no --organization DUT')
-if status != 0: print('===>>> batch_harvest_tud.py: error while executing previous script, status: ' + str(status) + '.'); exit(status)
+if status != 0: print('===>>> batch_harvest_dut.py: error while executing previous script, status: ' + str(status) + '.'); exit(status)
 
