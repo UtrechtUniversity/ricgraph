459c24ea2019b70a77af1e6b26ce76573e430280
Rik D.T. Janssen <121875841+RikDTJanssen@users.noreply.github.com>
2024-11-06 09:53:50 +0100

    Added code to explain what the script does and some tests to prevent accidental execution

--- maintenance_scripts/update_version.bash
+++ maintenance_scripts/update_version.bash
@@ -5,23 +5,45 @@
 # Rik D.T. Janssen, November 2024.
 # ###############################################################
 
-# Future extension:
-# Read the new version from an environment variable or from a VERSION file
-# NEW_VERSION=${1:-$(cat VERSION)}
+expected_run_directory="maintenance_scripts"
+
+echo "This script propagates a new version number to a number of files."
+if echo -n "Are you sure you want to proceed? [y/N] " && read ans && ! [ "${ans:-N}" = "y" ]; then
+    echo "Script aborted."
+    echo ""
+    exit 1
+fi
+
+current_dir=$(pwd)
+dir_name=$(basename "$current_dir")
+if [ "$dir_name" != "$expected_run_directory" ]; then
+    echo ""
+    echo "Error, this script is expected to run from directory \"$expected_run_directory\","
+    echo "but it is run from directory \"$current_dir\"."
+    echo "Please change directory. Exiting."
+    echo ""
+    exit 1
+fi
 
 # Get the new version from the user
+echo ""
 read -p "Specify the new version number: " new_version
 if [ -z "$new_version" ]; then
     echo "You have not specified a version number, exiting."
+    echo ""
     exit 1
 else
     echo "The new version number is: $new_version."
+    echo ""
 fi
 
-cd ..
-current_date=$(date '+%Y-%m-%d')
+# Possible future extension:
+# Read the new version from an environment variable or from a VERSION file
+# NEW_VERSION=${1:-$(cat VERSION)}
 
-# Future extension:
+cd ..
+
+# Possible future extension:
 # Update setup.py
 #echo "Updating setup.py..."
 #sed -i "s/version='.*'/version='${new_version}'/" setup.py
@@ -31,6 +53,7 @@ echo "Updating ricgraph/__init__.py..."
 sed -i "s/__version__ = '.*'/__version__ = '${new_version}'/" ricgraph/__init__.py
 
 # Update CITATION.cff
+current_date=$(date '+%Y-%m-%d')
 echo "Updating CITATION.cff..."
 sed -i "s/^version: .*/version: \"${new_version}\"/" CITATION.cff
 sed -i "s/date-released: .*/date-released: \"${current_date}\"/" CITATION.cff
