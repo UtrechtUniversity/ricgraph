718fd98f3816280d57f73d77358d9c276ebfd8bd
Rik D.T. Janssen <121875841+RikDTJanssen@users.noreply.github.com>
2026-04-30 17:36:35 +0200

    Added correct dependencies to pyproject.toml (= pyproject.toml-ricgraph) for ricgraph PyPI module; created pyproject.toml-ricgraph_explorer for (future) ricgraph_explorer PiPI module

---
+++ ricgraph_explorer/__init__.py
@@ -0,0 +1,53 @@
+# ########################################################################
+#
+# Ricgraph - Research in context graph
+#
+# ########################################################################
+#
+# MIT License
+#
+# Copyright (c) 2023 - 2026 Rik D.T. Janssen
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# ########################################################################
+#
+# This file is the __init__ file for Ricgraph Explorer.
+# For more information about Ricgraph and Ricgraph Explorer,
+# go to https://www.ricgraph.eu and https://docs.ricgraph.eu.
+#
+# ########################################################################
+#
+# Original version Rik D.T. Janssen, May 2026.
+#
+# ########################################################################
+
+from .ricgraph_explorer_constants import *
+from .ricgraph_explorer_init import *
+from .ricgraph_explorer_utils import *
+from .ricgraph_explorer_cypher import *
+from .ricgraph_explorer_graphdb import *
+from .ricgraph_explorer_javascript import *
+from .ricgraph_explorer_table import *
+from .ricgraph_explorer_restapi import *
+from .ricgraph_explorer_datavis import *
+from .ricgraph_explorer_collabs import *
+from .ricgraph_explorer_osl import *
+from .ricgraph_explorer_topics import *
+from .ricgraph_explorer import *
