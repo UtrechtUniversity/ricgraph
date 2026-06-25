9a3d7be074ad5349173fdf45d7cd9796b6f735a8
Rik D.T. Janssen <121875841+RikDTJanssen@users.noreply.github.com>
2026-06-25 13:15:43 +0200

    Added scripts to check for missing internal and external links on www.ricgraph.eu and docs.ricgraph.eu, removed old scripts that intended to do the same (but did not)

---
+++ maintenance/check_links_docs.ricgraph.eu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# ############################################################
+# Check whether all the internal and external links of the
+# Ricgraph website docs.ricgraph.eu are satisfied.
+#
+# For more information see the linkchecker module conf file
+# ricgraph_linkchecker.conf in this directory.
+#
+# Original version Rik D.T. Janssen, June 2026.
+# ############################################################
+
+linkchecker --config=ricgraph-linkchecker.conf https://docs.ricgraph.eu
+
+echo ""
+echo "For the errors or warnings reported above,"
+echo "note that links that go through Cloudflare and the like"
+echo "will be reported as errors. They will report"
+echo "'Result Error: 403 Forbidden', while they are correct."
+echo "This happens e.g. for DOI links."
+echo ""
---
+++ maintenance/check_links_www.ricgraph.eu_local.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# ############################################################
+# Check whether all the internal and external links of the
+# Ricgraph website www.ricgraph.eu (local) are satisfied.
+#
+# For more information see the linkchecker module conf file
+# ricgraph_linkchecker.conf in this directory.
+#
+# Original version Rik D.T. Janssen, June 2026.
+# ############################################################
+
+linkchecker --config=ricgraph-linkchecker.conf ../../ricgraph-documentation/build_website/index.html
+
+echo ""
+echo "For the errors or warnings reported above,"
+echo "note that links that go through Cloudflare and the like"
+echo "will be reported as errors. They will report"
+echo "'Result Error: 403 Forbidden', while they are correct."
+echo "This happens e.g. for DOI links."
+echo ""
